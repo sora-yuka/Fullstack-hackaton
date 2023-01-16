b508b8ac5bf5f22bdb5a3251f8426c4b0077e869
sora-yuka <feelingjeez@gmail.com>
2023-01-16 20:14:02 +0600

    account models - 1.0

--- applications/account/models.py
+++ applications/account/models.py
@@ -4,6 +4,47 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.hashers import make_password
 
 
+    
+GENDER = (
+    ("Female", "Female"),
+    ("Male", "Male"),
+    ("Indefined", "Indefined"),
+)
+
+
+# class UserManager(BaseUserManager):
+#     use_in_migrations = True
+
+#     def _create_user(self, username, email, password, **extra_fields):
+#         if not username:
+#             raise ValueError("The given username must be set")
+#         if not email:
+#             raise ValueError("The given email must be set")
+#         email = self.normalize_email(email)
+#         user = self.model(username=username, email=email, **extra_fields)
+#         user.password = make_password(password)
+#         user.activation_code()
+#         user.save(using=self._db)
+#         return user
+
+#     def create_user(self, username, email, password, **extra_fields):
+#         extra_fields.setdefault("is_staff", False)
+#         extra_fields.setdefault("is_superuser", False)
+#         extra_fields.setdefault("is_active", False)
+#         return self._create_user(username, email, password, **extra_fields)
+
+#     def create_superuser(self, username, email=None, password=None, **extra_fields):
+#         extra_fields.setdefault("is_staff", True)
+#         extra_fields.setdefault("is_superuser", True)
+#         extra_fields.setdefault("is_active", True)
+
+#         if extra_fields.get("is_staff") is not True:
+#             raise ValueError("Superuser must have is_staff=True.")
+#         if extra_fields.get("is_superuser") is not True:
+#             raise ValueError("Superuser must have is_superuser=True.")
+
+#         return self._create_user(username, email, password, **extra_fields)
+
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
@@ -35,49 +76,9 @@ class UserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
     
     
-GENDER = (
-    ("Female", "Female"),
-    ("Male", "Male"),
-    ("Indefined", "Indefined"),
-)
-
-
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def _create_user(self, username, email, password, **extra_fields):
-        if not username:
-            raise ValueError("The given username must be set")
-        if not email:
-            raise ValueError("The given email must be set")
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.password = make_password(password)
-        user.activation_code()
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, username, email, password, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        extra_fields.setdefault("is_active", False)
-        return self._create_user(username, email, password, **extra_fields)
-
-    def create_superuser(self, username, email=None, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self._create_user(username, email, password, **extra_fields)
-
-    
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=75)
+    # username = models.CharField(max_length=75)
+    username = None
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
     bank_card = models.PositiveIntegerField(min_length=16)
