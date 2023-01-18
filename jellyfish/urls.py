b9be74dc9b056940eead4e752addd6755ba7ec36
uulukbek <ulanovulukbek2@gmail.com>
2023-01-18 19:48:27 +0600

    changed media_root

--- jellyfish/urls.py
+++ jellyfish/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from django.conf.urls.static import static
+from jellyfish import settings
 
 schema_view = get_schema_view(openapi.Info(
     title = "Jellyfish",
@@ -32,3 +34,9 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger')),
     path("api/v1/account/", include("applications.account.urls"))
 ]
+
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
